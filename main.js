f239ff0c1cdc0f73eb841e9ce7ef6b915ffb4292
Pawan Deep Naugain <pawan.officeal@gmail.com>
2024-02-12 10:29:28 -0600

    adding main files in git

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const section1 = document.getElementById("section1");
+
+btn.addEventListener("click" , ()=>{
+    section1.classList.toggle("changeBGsection1");
+})
